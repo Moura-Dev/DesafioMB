ce329552e7ffa7916c64b5844841570ae9efd87b
Moura-Dev <junior.moura19@hotmail.com>
2020-11-29 01:19:31 -0300

    Crud Api

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from .views import testando
+from .views import BuscarFeira, BuscafeiraPorId, DeletarFeira, AtualizarFeira, AdicionarFeira
 from flask import Blueprint, jsonify, request
 from . import db
 from .models import FeirasLivres
@@ -7,7 +7,28 @@ from .models import FeirasLivres
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
-def teste():
-    return testando()
+@main.route('/feira/')
+def GetFeira():
+    return BuscarFeira()
+
+
+@main.route('/feira/<cod>')
+def GetFeiraDistrito(cod):
+    return BuscafeiraPorId(cod)
+
+
+@main.route('/feira/<cod>/', methods=['DELETE'])
+def DeleteFeira(cod):
+    return DeletarFeira(cod)
+
+
+@main.route('/feira/<cod>/', methods=['PUT', 'PATCH'])
+def AtualizarFeiraLivre(cod):
+    return AtualizarFeira(cod)
+
+
+@main.route('/feira/', methods=['POST'])
+def CriarFeira():
+    return AdicionarFeira()
+
 
--- app/views.py
+++ app/views.py
@@ -1,10 +1,82 @@
-from flask import jsonify
+from flask import jsonify, request
+from .models import FeirasLivres
+from . import db
 
 
-def teste():
-    return testando()
+def BuscarFeira():
+
+    consultar = FeirasLivres.query.all()
+    feiras = []
+    if not consultar:
+        return jsonify({'Message': "Não Existe Registros"}), 404
 
 
+    for f in consultar:
+        feiras.append({'id': f.id, 'long_': f.long_, 'lat': f.lat,'setcens': f.setcens, 
+        'areap': f.areap, 'coddist': f.coddist,'distrito': f.distrito, 'codsubprefe': f.codsubprefe, 
+        'subprefe': f.subprefe,'regiao5': f.regiao5, 'regiao8': f.regiao8, 'nome_feira': f.nome_feira,
+        'registro': f.registro,'logradouro': f.logradouro, 'numero': f.numero,
+        'bairro': f.bairro, 'referencia': f.referencia})
 
-def testando():
-    return jsonify('helaaaalow'), 200
+
+    return jsonify(feiras), 200
+
+   
+def BuscafeiraPorId(cod):
+    consultar = FeirasLivres.query.filter(FeirasLivres.id == cod)
+    feiras = []
+    
+    if consultar is None:
+        return jsonify({'Message': "Não Existe Registros"}), 404
+
+
+    for f in consultar:
+
+        feiras.append({'id': f.id, 'long_': f.long_, 'lat': f.lat,'setcens': f.setcens, 'areap': f.areap, 
+        'coddist': f.coddist,'distrito': f.distrito, 'codsubprefe': f.codsubprefe, 'subprefe': f.subprefe,
+        'regiao5': f.regiao5, 'regiao8': f.regiao8, 'nome_feira': f.nome_feira,'registro': f.registro,
+        'logradouro': f.logradouro, 'numero': f.numero,'bairro': f.bairro, 'referencia': f.referencia})
+
+        return jsonify(feiras), 200
+
+
+def DeletarFeira(cod):
+    FeirasLivres.query.filter(FeirasLivres.id == cod).delete()
+    try:
+        db.session.commit()
+        return jsonify({'Message': "Feira Deletada com Sucesso!!"}), 201
+
+    except:
+        return jsonify({'message': "Erro ao Deletar feira!"}), 404
+
+
+def AtualizarFeira(cod):
+    query = FeirasLivres.query.filter(FeirasLivres.id == cod)
+    try:
+        query.update(request.json)
+        db.session.commit()
+        return jsonify({'Message': "Feira Atualizada com Sucesso!!"}), 201
+
+    except:
+        return jsonify({'message': "Erro ao Atualizar Feira!!"}), 404
+
+
+def AdicionarFeira():
+
+    new_fr = request.get_json()
+
+    nova_feira = FeirasLivres(long_=new_fr['long_'], lat=new_fr['lat'],setcens=new_fr['setcens'], 
+    areap=new_fr['areap'],coddist=new_fr['coddist'], distrito=new_fr['distrito'],
+    codsubprefe=new_fr['codsubprefe'], subprefe=new_fr['subprefe'],regiao5=new_fr['regiao5'], 
+    regiao8=new_fr['regiao8'],nome_feira=new_fr['nome_feira'], registro=new_fr['registro'],
+    logradouro=new_fr['logradouro'], numero=new_fr['numero'],bairro=new_fr['bairro'], referencia=new_fr['referencia'],)
+    
+    
+    try:
+        db.session.add(nova_feira)
+        db.session.commit()
+        return jsonify({'Message': "Feira Criado com Sucesso!!"}), 201
+
+
+    except:
+        return jsonify({'message': "Erro ao Criar Feira!!"}), 404
