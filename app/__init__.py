c0e4dfb9c70c60b000f4606500cd896494170216
Moura-Dev <junior.moura19@hotmail.com>
2020-11-29 01:38:45 -0300

    Error Log

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from logging import FileHandler, WARNING
 
 db = SQLAlchemy()
 
@@ -8,6 +9,10 @@ db = SQLAlchemy()
 def create_app():
 
     app = Flask(__name__)
+    file_handler = FileHandler('errorlog.txt')
+    file_handler.setLevel(WARNING)
+    
+
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://moura:itnisan19@localhost/api'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -18,8 +23,7 @@ def create_app():
 
     from .routes import main
     app.register_blueprint(main)
-
-    app.register_blueprint(main)
+    app.logger.addHandler(file_handler)
 
 
     
